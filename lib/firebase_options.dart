013ae3a72de8f133eabca0ccfa8d0f91134d0347
dath7 <hoangdat24112002@gmail.com>
2024-12-26 18:02:31 +0700

    done

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -15,37 +15,37 @@ import 'package:flutter/foundation.dart'
 /// );
 /// ```
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI againdasdasd.1231',
-      );
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure thssssis byss running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configuredsssss for linux - '
-          'you can reconaafigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
+  // static FirebaseOptions get currentPlatform {
+  //   if (kIsWeb) {
+  //     throw UnsupportedError(
+  //       'DefaultFirebaseOptions have not been configured for web - '
+  //       'you can reconfigure this by running the FlutterFire CLI againdasdasd.1231',
+  //     );
+  //   }
+  //   switch (defaultTargetPlatform) {
+  //     case TargetPlatform.android:
+  //       return android;
+  //     case TargetPlatform.iOS:
+  //       return ios;
+  //     case TargetPlatform.macOS:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for macos - '
+  //         'you can reconfigure this by running the FlutterFire CLI again.',
+  //       );
+  //     case TargetPlatform.windows:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configured for windows - '
+  //         'you can reconfigure thssssis byss running the FlutterFire CLI again.',
+  //       );
+  //     case TargetPlatform.linux:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions have not been configuredsssss for linux - '
+  //         'you can reconaafigure this by running the FlutterFire CLI again.',
+  //       );
+  //     default:
+  //       throw UnsupportedError(
+  //         'DefaultFirebaseOptions are not supported for this platform.',
+  //       );
     }
   }
 
