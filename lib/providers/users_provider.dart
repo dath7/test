32d819118d241513a311441db15ee0bf81094579
dath7 <hoangdat24112002@gmail.com>
2024-12-27 10:55:57 +0700

    #43 test

--- lib/providers/users_provider.dart
+++ lib/providers/users_provider.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+sssss
 // state user: login logout
 final userProvider = StreamProvider<User?>((ref) {
   return FirebaseAuth.instance.authStateChanges();
